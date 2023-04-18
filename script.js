09f316ee3c01ad8f982e2e623aa5befd6e29051e
Zando411 <123271994+Zando411@users.noreply.github.com>
2023-04-17 21:55:52 -0700

    add div and player creation

--- script.js
+++ script.js
@@ -1,5 +1,43 @@
 const Gameboard = (() => {
   let gameboard = ['', '', '', '', '', '', '', '', ''];
+
+  const render = () => {
+    let boardHTML = '';
+    gameboard.forEach((cell, index) => {
+      boardHTML += `<div class="cell" id=square-${index}>${cell}</div>`;
+    });
+    document.querySelector('#gameboard').innerHTML = boardHTML;
+  };
+
+  return {
+    render,
+  };
+})();
+
+const CreatePlayer = (name, mark) => {
+  return {
+    name,
+    mark,
+  };
+};
+
+const Game = (() => {
+  let players = [];
+  let currentPlayerIndex;
+  let gameOver;
+
+  const start = () => {
+    players = [
+      CreatePlayer(document.querySelector('#player1').value, 'X'),
+      CreatePlayer(document.querySelector('#player2').value, 'O'),
+    ];
+    currentPlayerIndex = 0;
+    gameOver = false;
+    Gameboard.render();
+  };
+  return {
+    start,
+  };
 })();
 
 const startButton = document.querySelector('#start-button');
